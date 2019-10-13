cdfaeac792b6c55be20109f7660f4735b2fc0f91
Tomasz Nowak <tn@mpro.pl>
2019-10-14 00:05:30 +0200

    connect server heroku

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-
-
-app.listen(PORT)
 
 app.use(bodyParser.json());
 
 require('./routes/dialogFlowRoutes')(app)
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT)
 
 
 
